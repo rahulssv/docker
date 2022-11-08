9cd96ddb3ee049a8b40ab847111a83fc0085a1a3
igorferreir4 <94236607+igorferreir4@users.noreply.github.com>
2022-11-08 01:36:55 -0300

    teste

---
+++ imagens/teste/config/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# terminate on errors
+set -e
+
+# Check if volume is empty
+if [ ! "$(ls -A "/var/www/wordpress" 2>/dev/null)" ]; then
+    echo 'Setting up wordpress volume'
+    # Copy Wordpress from /tmp src to volume
+    cp /tmp/wordpress/* /var/www/wordpress/
+    chown -R caddy.caddy /var/www
+
+    # Generate secrets
+    curl -f https://api.wordpress.org/secret-key/1.1/salt/ >> /var/www/wordpress/wp-secrets.php
+
+    # Clean /tmp folder
+    rm -r /tmp/*
+fi
+
+usermod --non-unique --uid $PUID caddy
+
+groupmod --non-unique --gid $PGID caddy
+
+chown -R caddy.caddy /var/www
+
+exec "$@"
