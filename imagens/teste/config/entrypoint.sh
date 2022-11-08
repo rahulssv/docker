2fa243bc6a972cdd7af54304802cdd05acbc9ac3
igorferreir4 <94236607+igorferreir4@users.noreply.github.com>
2022-11-08 02:34:41 -0300

    teste

--- imagens/teste/config/entrypoint.sh
+++ imagens/teste/config/entrypoint.sh
@@ -3,6 +3,12 @@
 # terminate on errors
 set -e
 
+usermod --non-unique --uid $PUID caddy
+
+groupmod --non-unique --gid $PGID caddy
+
+chown -R caddy.caddy /var/www
+
 # Check if volume is empty
 if [ ! "$(ls -A "/var/www/wordpress" 2>/dev/null)" ]; then
     echo 'Setting up wordpress volume'
@@ -12,15 +18,6 @@ if [ ! "$(ls -A "/var/www/wordpress" 2>/dev/null)" ]; then
 
     # Generate secrets
     curl -f https://api.wordpress.org/secret-key/1.1/salt/ >> /var/www/wordpress/wp-secrets.php
-
-    # Clean /tmp folder
-    rm -r /tmp/*
 fi
-
-usermod --non-unique --uid $PUID caddy
-
-groupmod --non-unique --gid $PGID caddy
-
-chown -R caddy.caddy /var/www
 
 exec "$@"
