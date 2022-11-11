980262471c977a2b45415b4b76686b44231e6957
igorferreir4 <94236607+igorferreir4@users.noreply.github.com>
2022-11-11 12:03:52 -0300

    teste

--- imagens/teste/3.3.8/files/entrypoint.sh
+++ imagens/teste/3.3.8/files/entrypoint.sh
@@ -3,12 +3,6 @@
 # terminate on errors
 set -e
 
-usermod --non-unique --uid $PUID phpbb
-
-groupmod --non-unique --gid $PGID phpbb
-
-chown -R phpbb.phpbb /phpbb
-
 # Check if volume is empty
 if [ ! "$(ls -A "/phpbb/www" 2>/dev/null)" ]; then
     echo 'Setting up phpbb volume'
@@ -19,5 +13,30 @@ if [ ! "$(ls -A "/phpbb/www" 2>/dev/null)" ]; then
     # Fix chown
     chown -R phpbb.phpbb /phpbb
 fi
+
+[[ "${PHPBB_INSTALL}" = "true" ]] && rm config.php
+[[ "${PHPBB_INSTALL}" != "true" ]] && rm -rf install
+
+db_wait() {
+        if [[ "${PHPBB_DB_WAIT}" = "true" &&  "${PHPBB_DB_DRIVER}" != "sqlite3" && "${PHPBB_DB_DRIVER}" != "sqlite" ]]; then
+            until nc -z ${PHPBB_DB_HOST} ${PHPBB_DB_PORT}; do
+                echo "$(date) - waiting for database on ${PHPBB_DB_HOST}:${PHPBB_DB_PORT} to start before applying migrations"
+                sleep 3
+            done
+        fi
+}
+
+db_migrate() {
+    if [[ "${PHPBB_DB_AUTOMIGRATE}" = "true" && "${PHPBB_INSTALL}" != "true" ]]; then
+        echo "$(date) - applying migrations"
+        su-exec phpbb php81 bin/phpbbcli.php db:migrate
+    fi
+}
+
+usermod --non-unique --uid $PUID phpbb
+
+groupmod --non-unique --gid $PGID phpbb
+
+chown -R phpbb.phpbb /phpbb
 
 exec "$@"
