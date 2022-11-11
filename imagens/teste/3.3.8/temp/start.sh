5e9fd6d66f70db226d444f47af6737158e064b14
igorferreir4 <94236607+igorferreir4@users.noreply.github.com>
2022-11-11 03:53:45 -0300

    teste

--- imagens/teste/3.3.8/temp/start.sh
+++ imagens/teste/3.3.8/temp/start.sh
@@ -11,20 +11,4 @@ set -e
 [[ "${PHPBB_INSTALL}" = "true" ]] && rm config.php
 [[ "${PHPBB_INSTALL}" != "true" ]] && rm -rf install
 
-db_wait() {
-        if [[ "${PHPBB_DB_WAIT}" = "true" &&  "${PHPBB_DB_DRIVER}" != "sqlite3" && "${PHPBB_DB_DRIVER}" != "sqlite" ]]; then
-            until nc -z ${PHPBB_DB_HOST} ${PHPBB_DB_PORT}; do
-                echo "$(date) - waiting for database on ${PHPBB_DB_HOST}:${PHPBB_DB_PORT} to start before applying migrations"
-                sleep 3
-            done
-        fi
-}
-
-db_migrate() {
-    if [[ "${PHPBB_DB_AUTOMIGRATE}" = "true" && "${PHPBB_INSTALL}" != "true" ]]; then
-        echo "$(date) - applying migrations"
-        su-exec php81 bin/phpbbcli.php db:migrate
-    fi
-}
-
-db_wait && db_migrate && exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf "$@"
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf "$@"
