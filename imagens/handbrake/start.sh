56e3a681ac6f197df7588e1e39f81716a069bf6f
igorferreir4 <94236607+igorferreir4@users.noreply.github.com>
2022-12-03 02:01:04 -0300

    test

--- imagens/handbrake/start.sh
+++ imagens/handbrake/start.sh
@@ -8,11 +8,11 @@ for f in **/*.{avi,AVI,ogm,OGM,wmv,WMV,flv,FLV,mkv,MKV,mov,MOV,m4v,M4V}; do
     printf '\033[1;34;40m'
     echo "Converting $f"
     printf '\033[0m'
-    HandBrakeCLI -i "$f" --preset "Very Fast 1080p30" "${f:0:-5}.mp4" && rm "$f"
+    HandBrakeCLI -i "$f" --preset "Very Fast 1080p30" -o "${f:0:-5}.mp4" && rm "$f"
 done
 for f in **/*.{divx,DIVX,webm,WEBM}; do
     printf '\033[1;34;40m'
     echo "Converting $f"
     printf '\033[0m'
-    HandBrakeCLI -i "$f" --preset "Very Fast 1080p30" "${f:0:-5}.mp4" && rm "$f"
+    HandBrakeCLI -i "$f" --preset "Very Fast 1080p30" -o "${f:0:-5}.mp4" && rm "$f"
 done
