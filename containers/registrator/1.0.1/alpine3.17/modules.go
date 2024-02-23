a6a7611bb946eb479d915aeac0d5f6aea8542808
Igor Ferreira <94236607+igorferreir4@users.noreply.github.com>
2024-02-23 18:48:15 -0300

    teste

--- containers/registrator/1.0.1/alpine3.17/modules.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	_ "github.com/mario-ezquerro/registrator/consul"
-	_ "github.com/mario-ezquerro/registrator/consulkv"
-	_ "github.com/mario-ezquerro/registrator/etcd"
-	_ "github.com/mario-ezquerro/registrator/skydns2"
-	_ "github.com/mario-ezquerro/registrator/zookeeper"
-)
