ac2b54e3483918f7cec18db99ef526f66d071d85
Igor Ferreira <94236607+igorferreir4@users.noreply.github.com>
2024-02-27 02:37:32 -0300

    teste

---
+++ containers/registrator/1.0.1/alpine3.19/teste.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker compose -f docker-compose.amd64 up -d consul-server
+sleep 10
+docker compose -f docker-compose.amd64 up -d registrator
+sleep 10
+docker logs consul-server
+echo "-----------------------------------------------------------"
+docker logs registrator >> ./logs-registrator.txt 2>&1
+cat ./logs-registrator.txt
+echo "-----------------------------------------------------------"
+awk '/Syncing services on/ { found=1; print "EXPRESSÃO ENCONTRADA!"; exit } END { if (!found) { print "ERRO, EXPRESSÃO NÃO ENCONTRADA!"; exit 1 } }' ./logs-registrator.txt
